73c28e9864dca51520c78a342d347c28521408a5
marble703 <703342335@qq.com>
2025-03-08 15:58:38 +0800

    数值类型 - 整数 - 浮点 - 序列 - 复数

--- document/basic_type.md
+++ document/basic_type.md
@@ -1,5 +1,7 @@
 # 基本类型
 
+[示例程序](../type/num_type/src/main.rs)
+
 ## 数据类型
 
 1. 数值类型
@@ -8,8 +10,10 @@
 - 无符号整`u8, u16, u32, u64, usize`
 - 浮点`f32, f64`
 - 复数` num::complex::Complex::new(10, 20) `
+- `size`取决于CPU64/32位 
+- 在数字中可以任意插入_,仅用于视觉分隔, 如1_2_3(很好奇这有啥用)
 
-2. 字符串
+1. 字符串
 
 - &str
 
@@ -23,4 +27,59 @@
 5. 单元类型
 - () ，其唯一的值也是 ()
 
+## 浮点精度
+
+float系列类型的比较运算使用`std::cmp::PartialEq`而非`std::cmp::Eq`，因为
+1. 精度: 浮点精度
+2. `NaN`: 根据[IEEE-754](https://wikipedia.org/wiki/IEEE_754)标准，`NaN`不满足自反性`(a == a) == true`
+
+- 解决方法
+`epsilon`
+```Rust
+ (0.1_f64 + 0.2 - 0.3).abs() < 0.00001
+```
+
+## 整型溢出
+
+- `--release` 下不检测溢出，按照补码循环溢出规则处理
+
+- 使用 `wrapping_*` 方法在所有模式下都按照补码循环溢出规则处理，例如 `wrapping_add`
+- 如果使用`checked_*` 方法时发生溢出，则返回 None 值
+- 使用 `overflowing_*` 方法返回该值和一个指示是否存在溢出的布尔值
+- 使用 `saturating_*` 方法，可以限定计算后的结果不超过目标类型的最大值或低于最小值
+
 ## 类型推导与标注
+
+类似C++
+
+整型默认i32
+
+`u8`可以使用字节赋值, 如`b'A'`
+
+## 位运算
+
+同C++
+
+## Range 序列
+
+### 连续序列
+
+只允许字符和数字
+
+```Rust
+    for i in 1..=5 {
+        println!("{}", i);
+    }
+```
+
+## 复数
+
+[示例程序](../type/complex-num/src/main.rs)
+
+需要引入`num`库
+
+```Rust
+use num::complex::Complex;
+```
+
+需要在`Cargo.toml`中`[dependencies]` 下添加一行 `num = "0.4.0"`
--- type/num_type/src/main.rs
+++ type/num_type/src/main.rs
@@ -1,3 +1,124 @@
 fn main() {
-    println!("Hello, world!");
+    // 处理溢出
+    assert_eq!(100u8.saturating_add(1), 101);
+    assert_eq!(u8::MAX.saturating_add(127), u8::MAX);
+
+    // 整型检查溢出
+
+    let a: u8 = 255;
+    let b = a.wrapping_add(20);
+    println!("{}", b); // 19
+
+    // 浮点数
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    // assert!(xyz.0 + xyz.1 == xyz.2);// 会报错
+
+    // NaN
+    let x = (-42.0_f32).sqrt();
+    if x.is_nan() {
+        println!("未定义的数学行为")
+    }
+
+    // 数学运算
+
+    // 加法
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+
+    // 减法
+    let difference = 95.5 - 4.3;
+    println!("difference: {}", difference);
+
+    // 乘法
+    let product = 4 * 30;
+    println!("product: {}", product);
+
+    // 除法
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+
+    // 求余
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
+
+    // 综合
+
+    // 编译器会进行自动推导，给予twenty i32的类型
+    let twenty = 20;
+    // 类型标注
+    let twenty_one: i32 = 21;
+    // 通过类型后缀的方式进行类型标注：22是i32类型
+    let twenty_two = 22i32;
+
+    // 只有同样类型，才能运算
+    let addition = twenty + twenty_one + twenty_two;
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
+
+    // 对于较长的数字，可以用_进行分割，提升可读性
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+
+    // 定义一个f32数组，其中42.0会自动被推导为f32类型
+    let forty_twos = [42.0, 42f32, 42.0_f32];
+
+    // 打印数组中第一个值，并控制小数位为2位
+    println!("{:.2}", forty_twos[0]);
+
+    // 位运算
+
+    // 无符号8位整数，二进制为00000010
+    let a: u8 = 2; // 也可以写 let a: u8 = 0b_0000_0010;
+
+    // 二进制为00000011
+    let b: u8 = 3;
+
+    // {:08b}：左高右低输出二进制01，不足8位则高位补0
+    println!("a value is        {:08b}", a);
+
+    println!("b value is        {:08b}", b);
+
+    println!("(a & b) value is  {:08b}", a & b);
+
+    println!("(a | b) value is  {:08b}", a | b);
+
+    println!("(a ^ b) value is  {:08b}", a ^ b);
+
+    println!("(!b) value is     {:08b}", !b);
+
+    println!("(a << b) value is {:08b}", a << b);
+
+    println!("(a >> b) value is {:08b}", a >> b);
+
+    let mut a = a;
+    // 注意这些计算符除了!之外都可以加上=进行赋值 (因为!=要用来判断不等于)
+    a <<= b;
+    println!("(a << b) value is {:08b}", a);
+
+    // 序列
+
+    for i in 1..=5 {
+        println!("{}", i);
+    }
+
+    println!();
+
+    for i in 'a'..='z' {
+        println!("{}", i);
+    }
 }
